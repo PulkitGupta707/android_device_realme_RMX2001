1f3ba1f517871ec6454ed4fdf3f2b099ffaa8ecf
TheMalachite <eliasgheeraert@gmail.com>
2020-08-04 08:28:36 +0200

    RMX2001: Initial commit

---
+++ lineage_RMX2001.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from picasso device
+$(call inherit-product, device/realme/RMX2001/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_RMX2001
+PRODUCT_DEVICE := RMX2001
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := realme 6
+PRODUCT_MANUFACTURER := realme
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Build info
+BUILD_FINGERPRINT := "realme/RMX2001/RMX2001L1:10/QP1A.190711.020/1594211000:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2001 \
+    PRODUCT_NAME=RMX2001 \
+    PRIVATE_BUILD_DESC="full_oppo6785-user 10 QP1A.190711.020 0b36b6c4f9b3bf3b release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
